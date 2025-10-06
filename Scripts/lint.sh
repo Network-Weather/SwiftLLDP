869de66a5d4124b72e5e8cba56711d072ced3ee3
David E. Weekly <david@weekly.org>
2025-10-06 15:24:33 -0700

    Fix swift lint to recursively lint.

--- Scripts/lint.sh
+++ Scripts/lint.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source "$SCRIPT_DIR/common.sh"
 
 if swift --help | grep -q "format"; then
-  swift format lint --strict "$PROJECT_ROOT/Sources" "$PROJECT_ROOT/Tests"
+  swift format lint --strict --recursive "$PROJECT_ROOT/Sources" "$PROJECT_ROOT/Tests"
 else
   if ! command -v swift-format >/dev/null; then
     echo "swift-format CLI not installed; see README for installation instructions" >&2
